223b52ab9d9f2d1e1aa4702001d3e69f222e2480
Claude-Blanchet-Babin <claude.bb@orange.fr>
2023-12-12 15:29:13 +0100

    12/12/2023
    + système spawn aléatoire

--- Assets/Script/enemyManager.cs
+++ Assets/Script/enemyManager.cs
@@ -12,17 +12,42 @@ public class enemyManager : MonoBehaviour
     private scriptableObjectEnemy currentEnemy;
     public int hp;
 
+    public float TimeSpawn;
+    public float timer;
+
+    public bool Enemyactiv;
+
+    /*public int RandomSpawn;
+    public Transform Parent;
+    public GameObject EnemyAnger;
+    public GameObject EnemyHate;
+    public int enemyLife;
+    */
+
 
     // Start is called before the first frame update
     void Start()
     {
-        EnemySpawn();
+        Enemyactiv = false;
+        TimeSpawn = Random.Range(3, 10);
+        //Invoke("EnemySpawn", TimeSpawn);
+
+        //Invoke("spawnTest", 5f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        timer += Time.deltaTime;
+        if (timer >= TimeSpawn && (Enemyactiv == false))
+        {
+            EnemySpawn();
+            timer = 0;
+        }
+
+
     }
 
     public void EnemySpawn()
@@ -31,8 +56,46 @@ public class enemyManager : MonoBehaviour
         currentEnemy = EnemyList[Random.Range(0, EnemyList.Length)];
 
         spriteRenderer.sprite = currentEnemy.Appareance;
+        spriteRenderer.enabled = true;
+        Enemyactiv = true;
         //nameEnemyUI.text = currentEnemy.EnemyName;
         hp = currentEnemy.Healpoint;
         //lifeEnemyUI.text = hp.ToString();
+    }
+
+    /*public void spawnTest()
+    {
+        RandomSpawn = Random.Range(0, 100 + 1);
+
+        if (RandomSpawn <= 75)
+        {
+
+            Instantiate(EnemyAnger, Parent.position, Parent.rotation);
+            enemyLife = 3;
+        }
+
+        if (RandomSpawn > 75 && RandomSpawn <= 100)
+        {
+            Instantiate(EnemyHate, Parent.position, Parent.rotation);
+            enemyLife = 5;
+        }
+    }*/
+
+    public void OnMouseDown()
+    {
+        if (Enemyactiv == true)
+        {
+            hp = hp - 1;
+        }
+
+        if (hp <= 0 && Enemyactiv == true)
+        {
+            spriteRenderer.enabled = false;
+            TimeSpawn = Random.Range(3, 10);
+            //Invoke("EnemySpawn", TimeSpawn);
+            Enemyactiv = false;
+            timer = 0;
+        }
+
     }
 }
